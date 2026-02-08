4083f7aa4a4da2dc757fb8582dcbba67de9c0358
Beniamino Squitieri <bennibeniamino@gmail.com>
2026-02-08 13:59:26 +0100

    Delete install directory

--- install/cartesian_control_msgs/share/cartesian_control_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(cartesian_control_msgs_IDL_FILES "action/FollowCartesianTrajectory.idl")
-set(cartesian_control_msgs_INTERFACE_FILES "action/FollowCartesianTrajectory.action")
